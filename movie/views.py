5b6f322b4805727f6ddbc3ad5ea8c45993eb3544
Horizon4 <jeronimogomezalzate3@gmail.com>
2026-02-07 19:29:46 -0500

    nuevo archivo about.hmtl

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
 from django.shortcuts import render
 
 def home(request):
     return render(request, "movie/home.html", {"name": "Jeronimo"})
+
+def about(request):
+    return render(request, "movie/about.html")
